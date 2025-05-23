0445e6d70f0b49c758b5b00b269281b4ff8fe129
LThompson21 <21lrthompson04@gmail.com>
2025-05-22 23:56:44 -0400

    Add project files.

---
+++ src/cpp/Item.cpp
@@ -0,0 +1,81 @@
+// external/std includes
+#include <string>
+#include <cmath>
+
+// internal includes
+#include "Item.h"
+
+Item::Item( const std::string& name, const std::string& description, double price )
+	: name( name ), description( description ), price( price ), quantity( 0 )
+{
+}
+
+Item::Item( const std::string& name, const std::string& description, double price, size_t quantity )
+	: name( name ), description( description ), price( price ), quantity( quantity )
+{
+}
+
+std::string Item::getName() const
+{
+	return this->name;
+}
+
+std::string Item::getDescription() const
+{
+	return this->description;
+}
+
+double Item::getPrice() const
+{
+	return this->price;
+}
+
+size_t Item::getQuantity() const
+{
+	return this->quantity;
+}
+
+void Item::setName( const std::string& name )
+{
+	this->name = name;
+}
+
+void Item::setDescription( const std::string& description )
+{
+	this->description = description;
+}
+
+void Item::setPrice( double price )
+{
+	this->price = price;
+}
+
+void Item::setQuantity( size_t quantity )
+{
+	this->quantity = quantity;
+}
+
+void Item::adjustQuantity( int amount )
+{
+	if ( amount < 0 )
+	{
+		const size_t subtractAmount = static_cast< size_t >( -amount );
+		if ( subtractAmount > this->quantity )
+		{
+			this->quantity = 0;
+		}
+		else
+		{
+			this->quantity -= subtractAmount;
+		}
+	}
+	else
+	{
+		this->quantity += amount;
+	}
+}
+
+size_t Item::getMaxStackQuantity() const
+{
+	return 100;
+}
---
+++ src/include/Item.h
@@ -0,0 +1,39 @@
+#pragma once
+
+// external/std includes
+#include <string>
+
+// internal includes
+
+class Item
+{
+public:
+	virtual std::string asString() const = 0;
+
+	std::string getName() const;
+	std::string getDescription() const;
+	double getPrice() const;
+	size_t getQuantity() const;
+
+	virtual size_t getMaxStackQuantity() const;
+
+	void setName( const std::string& name );
+	void setDescription( const std::string& description );
+	void setPrice( double price );
+
+	void adjustQuantity( int amount );
+
+protected:
+	Item( const std::string& name, const std::string& description, double price );
+	Item( const std::string& name, const std::string& description, double price, size_t quantity );
+
+	virtual ~Item() = default;
+
+	void setQuantity( size_t quantity );
+
+private:
+	std::string name;
+	std::string description;
+	double price;
+	size_t quantity;
+};
